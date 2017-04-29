f8ce32e9e502b64d2ee42a846657dd28a6e9cb4b
Carlos Quiroz <carlos.m.quiroz@gmail.com>
2017-04-28 21:10:51 -0300

    Port TestDateTimeBuilderCombinations to scalatest

--- shared/src/test/scala/org/threeten/bp/format/TestDateTimeBuilderCombinations.scala
+++ shared/src/test/scala/org/threeten/bp/format/TestDateTimeBuilderCombinations.scala
@@ -31,7 +31,8 @@
  */
 package org.threeten.bp.format
 
-import org.testng.Assert.assertEquals
+import org.scalatest.FunSuite
+import org.threeten.bp.AssertionsHelper
 import org.threeten.bp.temporal.ChronoField.ALIGNED_DAY_OF_WEEK_IN_MONTH
 import org.threeten.bp.temporal.ChronoField.ALIGNED_DAY_OF_WEEK_IN_YEAR
 import org.threeten.bp.temporal.ChronoField.ALIGNED_WEEK_OF_MONTH
@@ -43,8 +44,6 @@ import org.threeten.bp.temporal.ChronoField.EPOCH_DAY
 import org.threeten.bp.temporal.ChronoField.MONTH_OF_YEAR
 import org.threeten.bp.temporal.ChronoField.PROLEPTIC_MONTH
 import org.threeten.bp.temporal.ChronoField.YEAR
-import org.testng.annotations.DataProvider
-import org.testng.annotations.Test
 import org.threeten.bp.Instant
 import org.threeten.bp.LocalDate
 import org.threeten.bp.ZoneId
@@ -63,58 +62,84 @@ object TestDateTimeBuilderCombinations {
   }
 }
 
-class TestDateTimeBuilderCombinations {
+class TestDateTimeBuilderCombinations extends FunSuite with AssertionsHelper {
   import TestDateTimeBuilderCombinations.localDateFrom
 
-  @DataProvider(name = "combine") private[format] def data_combine: Array[Array[Any]] = {
-    Array[Array[Any]](Array(YEAR, 2012, MONTH_OF_YEAR, 6, DAY_OF_MONTH, 3, null, null, localDateFrom, LocalDate.of(2012, 6, 3)), Array(PROLEPTIC_MONTH, 2012 * 12 + 6 - 1, DAY_OF_MONTH, 3, null, null, null, null, localDateFrom, LocalDate.of(2012, 6, 3)), Array(YEAR, 2012, ALIGNED_WEEK_OF_YEAR, 6, DAY_OF_WEEK, 3, null, null, localDateFrom, LocalDate.of(2012, 2, 8)), Array(YEAR, 2012, DAY_OF_YEAR, 155, null, null, null, null, localDateFrom, LocalDate.of(2012, 6, 3)), Array(EPOCH_DAY, 12, null, null, null, null, null, null, localDateFrom, LocalDate.of(1970, 1, 13)))
+  val data_combine: List[(TemporalField, Number, TemporalField, Number, TemporalField, Number, TemporalField, Number, TemporalQuery[LocalDate], AnyRef)] = {
+    List(
+      (YEAR, 2012, MONTH_OF_YEAR, 6, DAY_OF_MONTH, 3, null, null, localDateFrom, LocalDate.of(2012, 6, 3)),
+      (PROLEPTIC_MONTH, 2012 * 12 + 6 - 1, DAY_OF_MONTH, 3, null, null, null, null, localDateFrom, LocalDate.of(2012, 6, 3)),
+      (YEAR, 2012, ALIGNED_WEEK_OF_YEAR, 6, DAY_OF_WEEK, 3, null, null, localDateFrom, LocalDate.of(2012, 2, 8)),
+      (YEAR, 2012, DAY_OF_YEAR, 155, null, null, null, null, localDateFrom, LocalDate.of(2012, 6, 3)),
+      (EPOCH_DAY, 12, null, null, null, null, null, null, localDateFrom, LocalDate.of(1970, 1, 13)))
   }
 
-  @Test(dataProvider = "combine") def test_derive(field1: TemporalField, value1: Number, field2: TemporalField, value2: Number, field3: TemporalField, value3: Number, field4: TemporalField, value4: Number, query: TemporalQuery[Any], expectedVal: AnyRef): Unit = {
-    val builder: DateTimeBuilder = new DateTimeBuilder(field1, value1.longValue)
-    builder.chrono = IsoChronology.INSTANCE
-    if (field2 != null) {
-      builder.addFieldValue(field2, value2.longValue)
-    }
-    if (field3 != null) {
-      builder.addFieldValue(field3, value3.longValue)
-    }
-    if (field4 != null) {
-      builder.addFieldValue(field4, value4.longValue)
-    }
-    builder.resolve(ResolverStyle.SMART, null)
-    assertEquals(builder.build(query), expectedVal)
+  test("test_derive") {
+    data_combine.foreach {
+      case (field1, value1, field2, value2, field3, value3, field4, value4, query, expectedVal) =>
+        val builder: DateTimeBuilder = new DateTimeBuilder(field1, value1.longValue)
+        builder.chrono = IsoChronology.INSTANCE
+        if (field2 != null) {
+          builder.addFieldValue(field2, value2.longValue)
+        }
+        if (field3 != null) {
+          builder.addFieldValue(field3, value3.longValue)
+        }
+        if (field4 != null) {
+          builder.addFieldValue(field4, value4.longValue)
+        }
+        builder.resolve(ResolverStyle.SMART, null)
+        assertEquals(builder.build(query), expectedVal)
+      case _ =>
+        fail()
+      }
   }
 
-  @DataProvider(name = "normalized") private[format] def data_normalized: Array[Array[Any]] = {
-    Array[Array[Any]](Array(YEAR, 2127, null, null, null, null, YEAR, 2127), Array(MONTH_OF_YEAR, 12, null, null, null, null, MONTH_OF_YEAR, 12), Array(DAY_OF_YEAR, 127, null, null, null, null, DAY_OF_YEAR, 127), Array(DAY_OF_MONTH, 23, null, null, null, null, DAY_OF_MONTH, 23), Array(DAY_OF_WEEK, 127, null, null, null, null, DAY_OF_WEEK, 127L), Array(ALIGNED_WEEK_OF_YEAR, 23, null, null, null, null, ALIGNED_WEEK_OF_YEAR, 23), Array(ALIGNED_DAY_OF_WEEK_IN_YEAR, 4, null, null, null, null, ALIGNED_DAY_OF_WEEK_IN_YEAR, 4L), Array(ALIGNED_WEEK_OF_MONTH, 4, null, null, null, null, ALIGNED_WEEK_OF_MONTH, 4), Array(ALIGNED_DAY_OF_WEEK_IN_MONTH, 3, null, null, null, null, ALIGNED_DAY_OF_WEEK_IN_MONTH, 3), Array(PROLEPTIC_MONTH, 15, null, null, null, null, PROLEPTIC_MONTH, null), Array(PROLEPTIC_MONTH, 1971 * 12 + 4 - 1, null, null, null, null, YEAR, 1971), Array(PROLEPTIC_MONTH, 1971 * 12 + 4 - 1, null, null, null, null, MONTH_OF_YEAR, 4))
+  val data_normalized: List[(TemporalField, Number, TemporalField, Number, TemporalField, Number, TemporalField, Number)] =
+    List(
+      (YEAR, 2127, null, null, null, null, YEAR, 2127),
+      (MONTH_OF_YEAR, 12, null, null, null, null, MONTH_OF_YEAR, 12),
+      (DAY_OF_YEAR, 127, null, null, null, null, DAY_OF_YEAR, 127),
+      (DAY_OF_MONTH, 23, null, null, null, null, DAY_OF_MONTH, 23),
+      (DAY_OF_WEEK, 127, null, null, null, null, DAY_OF_WEEK, 127L),
+      (ALIGNED_WEEK_OF_YEAR, 23, null, null, null, null, ALIGNED_WEEK_OF_YEAR, 23),
+      (ALIGNED_DAY_OF_WEEK_IN_YEAR, 4, null, null, null, null, ALIGNED_DAY_OF_WEEK_IN_YEAR, 4L),
+      (ALIGNED_WEEK_OF_MONTH, 4, null, null, null, null, ALIGNED_WEEK_OF_MONTH, 4),
+      (ALIGNED_DAY_OF_WEEK_IN_MONTH, 3, null, null, null, null, ALIGNED_DAY_OF_WEEK_IN_MONTH, 3),
+      (PROLEPTIC_MONTH, 15, null, null, null, null, PROLEPTIC_MONTH, null),
+      (PROLEPTIC_MONTH, 1971 * 12 + 4 - 1, null, null, null, null, YEAR, 1971),
+      (PROLEPTIC_MONTH, 1971 * 12 + 4 - 1, null, null, null, null, MONTH_OF_YEAR, 4))
+
+  test("test_normalized") {
+    data_normalized.foreach {
+      case (field1, value1, field2, value2, field3, value3, query, expectedVal) =>
+        val builder: DateTimeBuilder = new DateTimeBuilder(field1, value1.longValue)
+        builder.chrono = IsoChronology.INSTANCE
+        if (field2 != null) {
+          builder.addFieldValue(field2, value2.longValue)
+        }
+        if (field3 != null) {
+          builder.addFieldValue(field3, value3.longValue)
+        }
+        builder.resolve(ResolverStyle.SMART, null)
+        if (expectedVal != null) {
+          assertEquals(builder.getLong(query), expectedVal.longValue)
+        }
+        else {
+          assertEquals(builder.isSupported(query), false)
+        }
+      case _ =>
+        fail()
+    }
   }
 
-  @Test(dataProvider = "normalized") def test_normalized(field1: TemporalField, value1: Number, field2: TemporalField, value2: Number, field3: TemporalField, value3: Number, query: TemporalField, expectedVal: Number): Unit = {
-    val builder: DateTimeBuilder = new DateTimeBuilder(field1, value1.longValue)
-    builder.chrono = IsoChronology.INSTANCE
-    if (field2 != null) {
-      builder.addFieldValue(field2, value2.longValue)
-    }
-    if (field3 != null) {
-      builder.addFieldValue(field3, value3.longValue)
-    }
-    builder.resolve(ResolverStyle.SMART, null)
-    if (expectedVal != null) {
-      assertEquals(builder.getLong(query), expectedVal.longValue)
-    }
-    else {
-      assertEquals(builder.isSupported(query), false)
-    }
-  }
-
-  @Test def test_parse_ZDT_withZone(): Unit = {
+  test("test_parse_ZDT_withZone") {
     val fmt: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").withZone(TestDateTimeBuilderCombinations.PARIS)
     val acc: TemporalAccessor = fmt.parse("2014-06-30 01:02:03")
     assertEquals(ZonedDateTime.from(acc), ZonedDateTime.of(2014, 6, 30, 1, 2, 3, 0, TestDateTimeBuilderCombinations.PARIS))
   }
 
-  @Test def test_parse_Instant_withZone(): Unit = {
+  test("test_parse_Instant_withZone") {
     val fmt: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").withZone(TestDateTimeBuilderCombinations.PARIS)
     val acc: TemporalAccessor = fmt.parse("2014-06-30 01:02:03")
     assertEquals(Instant.from(acc), ZonedDateTime.of(2014, 6, 30, 1, 2, 3, 0, TestDateTimeBuilderCombinations.PARIS).toInstant)
