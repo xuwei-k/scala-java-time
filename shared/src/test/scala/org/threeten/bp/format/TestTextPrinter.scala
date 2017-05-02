40479932f1374f9a9d4b9dbc88622f62c6bc8d0f
Carlos Quiroz <carlos.m.quiroz@gmail.com>
2017-05-02 15:18:50 -0300

    Port TestTextPrinter to scalatest

--- shared/src/test/scala/org/threeten/bp/format/TestTextPrinter.scala
+++ shared/src/test/scala/org/threeten/bp/format/TestTextPrinter.scala
@@ -31,14 +31,13 @@
  */
 package org.threeten.bp.format
 
-import org.testng.Assert.assertEquals
 import org.threeten.bp.temporal.ChronoField.DAY_OF_MONTH
 import org.threeten.bp.temporal.ChronoField.DAY_OF_WEEK
 import org.threeten.bp.temporal.ChronoField.MONTH_OF_YEAR
 import java.util.Locale
 
-import org.testng.annotations.DataProvider
-import org.testng.annotations.Test
+import org.scalatest.FunSuite
+import org.threeten.bp.AssertionsHelper
 import org.threeten.bp.DateTimeException
 import org.threeten.bp.LocalDate
 import org.threeten.bp.format.internal.TTBPDateTimeFormatterBuilder
@@ -50,16 +49,15 @@ object TestTextPrinter {
   private val PROVIDER: DateTimeTextProvider = DateTimeTextProvider.getInstance
 }
 
-@Test class TestTextPrinter extends AbstractTestPrinterParser {
-  @Test(expectedExceptions = Array(classOf[DateTimeException]))
-  @throws(classOf[Exception])
-  def test_print_emptyCalendrical(): Unit = {
-    val pp: TTBPDateTimeFormatterBuilder.TextPrinterParser = new TTBPDateTimeFormatterBuilder.TextPrinterParser(DAY_OF_WEEK, TextStyle.FULL, TestTextPrinter.PROVIDER)
-    pp.print(printEmptyContext, buf)
+class TestTextPrinter extends FunSuite with GenTestPrinterParser with AssertionsHelper {
+  test("test_print_emptyCalendrical") {
+    assertThrows[DateTimeException] {
+      val pp: TTBPDateTimeFormatterBuilder.TextPrinterParser = new TTBPDateTimeFormatterBuilder.TextPrinterParser(DAY_OF_WEEK, TextStyle.FULL, TestTextPrinter.PROVIDER)
+      pp.print(printEmptyContext, buf)
+    }
   }
 
-  @throws(classOf[Exception])
-  def test_print_append(): Unit = {
+  test("test_print_append") {
     printContext.setDateTime(LocalDate.of(2012, 4, 18))
     val pp: TTBPDateTimeFormatterBuilder.TextPrinterParser = new TTBPDateTimeFormatterBuilder.TextPrinterParser(DAY_OF_WEEK, TextStyle.FULL, TestTextPrinter.PROVIDER)
     buf.append("EXISTING")
@@ -67,21 +65,56 @@ object TestTextPrinter {
     assertEquals(buf.toString, "EXISTINGWednesday")
   }
 
-  @DataProvider(name = "print") private[format] def provider_dow: Array[Array[Any]] = {
-    Array[Array[Any]](Array(DAY_OF_WEEK, TextStyle.FULL, 1, "Monday"), Array(DAY_OF_WEEK, TextStyle.FULL, 2, "Tuesday"), Array(DAY_OF_WEEK, TextStyle.FULL, 3, "Wednesday"), Array(DAY_OF_WEEK, TextStyle.FULL, 4, "Thursday"), Array(DAY_OF_WEEK, TextStyle.FULL, 5, "Friday"), Array(DAY_OF_WEEK, TextStyle.FULL, 6, "Saturday"), Array(DAY_OF_WEEK, TextStyle.FULL, 7, "Sunday"), Array(DAY_OF_WEEK, TextStyle.SHORT, 1, "Mon"), Array(DAY_OF_WEEK, TextStyle.SHORT, 2, "Tue"), Array(DAY_OF_WEEK, TextStyle.SHORT, 3, "Wed"), Array(DAY_OF_WEEK, TextStyle.SHORT, 4, "Thu"), Array(DAY_OF_WEEK, TextStyle.SHORT, 5, "Fri"), Array(DAY_OF_WEEK, TextStyle.SHORT, 6, "Sat"), Array(DAY_OF_WEEK, TextStyle.SHORT, 7, "Sun"), Array(DAY_OF_MONTH, TextStyle.FULL, 1, "1"), Array(DAY_OF_MONTH, TextStyle.FULL, 2, "2"), Array(DAY_OF_MONTH, TextStyle.FULL, 3, "3"), Array(DAY_OF_MONTH, TextStyle.FULL, 28, "28"), Array(DAY_OF_MONTH, TextStyle.FULL, 29, "29"), Array(DAY_OF_MONTH, TextStyle.FULL, 30, "30"), Array(DAY_OF_MONTH, TextStyle.FULL, 31, "31"), Array(DAY_OF_MONTH, TextStyle.SHORT, 1, "1"), Array(DAY_OF_MONTH, TextStyle.SHORT, 2, "2"), Array(DAY_OF_MONTH, TextStyle.SHORT, 3, "3"), Array(DAY_OF_MONTH, TextStyle.SHORT, 28, "28"), Array(DAY_OF_MONTH, TextStyle.SHORT, 29, "29"), Array(DAY_OF_MONTH, TextStyle.SHORT, 30, "30"), Array(DAY_OF_MONTH, TextStyle.SHORT, 31, "31"), Array(MONTH_OF_YEAR, TextStyle.FULL, 1, "January"), Array(MONTH_OF_YEAR, TextStyle.FULL, 12, "December"), Array(MONTH_OF_YEAR, TextStyle.SHORT, 1, "Jan"), Array(MONTH_OF_YEAR, TextStyle.SHORT, 12, "Dec"))
+  val provider_dow: List[(TemporalField, TextStyle, Int, String)] = {
+    List(
+      (DAY_OF_WEEK, TextStyle.FULL, 1, "Monday"),
+      (DAY_OF_WEEK, TextStyle.FULL, 2, "Tuesday"),
+      (DAY_OF_WEEK, TextStyle.FULL, 3, "Wednesday"),
+      (DAY_OF_WEEK, TextStyle.FULL, 4, "Thursday"),
+      (DAY_OF_WEEK, TextStyle.FULL, 5, "Friday"),
+      (DAY_OF_WEEK, TextStyle.FULL, 6, "Saturday"),
+      (DAY_OF_WEEK, TextStyle.FULL, 7, "Sunday"),
+      (DAY_OF_WEEK, TextStyle.SHORT, 1, "Mon"),
+      (DAY_OF_WEEK, TextStyle.SHORT, 2, "Tue"),
+      (DAY_OF_WEEK, TextStyle.SHORT, 3, "Wed"),
+      (DAY_OF_WEEK, TextStyle.SHORT, 4, "Thu"),
+      (DAY_OF_WEEK, TextStyle.SHORT, 5, "Fri"),
+      (DAY_OF_WEEK, TextStyle.SHORT, 6, "Sat"),
+      (DAY_OF_WEEK, TextStyle.SHORT, 7, "Sun"),
+      (DAY_OF_MONTH, TextStyle.FULL, 1, "1"),
+      (DAY_OF_MONTH, TextStyle.FULL, 2, "2"),
+      (DAY_OF_MONTH, TextStyle.FULL, 3, "3"),
+      (DAY_OF_MONTH, TextStyle.FULL, 28, "28"),
+      (DAY_OF_MONTH, TextStyle.FULL, 29, "29"),
+      (DAY_OF_MONTH, TextStyle.FULL, 30, "30"),
+      (DAY_OF_MONTH, TextStyle.FULL, 31, "31"),
+      (DAY_OF_MONTH, TextStyle.SHORT, 1, "1"),
+      (DAY_OF_MONTH, TextStyle.SHORT, 2, "2"),
+      (DAY_OF_MONTH, TextStyle.SHORT, 3, "3"),
+      (DAY_OF_MONTH, TextStyle.SHORT, 28, "28"),
+      (DAY_OF_MONTH, TextStyle.SHORT, 29, "29"),
+      (DAY_OF_MONTH, TextStyle.SHORT, 30, "30"),
+      (DAY_OF_MONTH, TextStyle.SHORT, 31, "31"),
+      (MONTH_OF_YEAR, TextStyle.FULL, 1, "January"),
+      (MONTH_OF_YEAR, TextStyle.FULL, 12, "December"),
+      (MONTH_OF_YEAR, TextStyle.SHORT, 1, "Jan"),
+      (MONTH_OF_YEAR, TextStyle.SHORT, 12, "Dec"))
   }
 
-  @Test(dataProvider = "print")
-  @throws(classOf[Exception])
-  def test_print(field: TemporalField, style: TextStyle, value: Int, expected: String): Unit = {
-    printContext.setDateTime(new MockFieldValue(field, value))
-    val pp: TTBPDateTimeFormatterBuilder.TextPrinterParser = new TTBPDateTimeFormatterBuilder.TextPrinterParser(field, style, TestTextPrinter.PROVIDER)
-    pp.print(printContext, buf)
-    assertEquals(buf.toString, expected)
+  test("test_print") {
+    provider_dow.foreach {
+      case (field, style, value, expected) =>
+        super.beforeEach()
+        printContext.setDateTime(new MockFieldValue(field, value))
+        val pp: TTBPDateTimeFormatterBuilder.TextPrinterParser = new TTBPDateTimeFormatterBuilder.TextPrinterParser(field, style, TestTextPrinter.PROVIDER)
+        pp.print(printContext, buf)
+        assertEquals(buf.toString, expected)
+      case _ =>
+        fail()
+    }
   }
 
-  @throws(classOf[Exception])
-  def test_print_french_long(): Unit = {
+  test("test_print_french_long") {
     printContext.setLocale(Locale.FRENCH)
     printContext.setDateTime(LocalDate.of(2012, 1, 1))
     val pp: TTBPDateTimeFormatterBuilder.TextPrinterParser = new TTBPDateTimeFormatterBuilder.TextPrinterParser(MONTH_OF_YEAR, TextStyle.FULL, TestTextPrinter.PROVIDER)
@@ -89,8 +122,7 @@ object TestTextPrinter {
     assertEquals(buf.toString, "janvier")
   }
 
-  @throws(classOf[Exception])
-  def test_print_french_short(): Unit = {
+  test("test_print_french_short") {
     printContext.setLocale(Locale.FRENCH)
     printContext.setDateTime(LocalDate.of(2012, 1, 1))
     val pp: TTBPDateTimeFormatterBuilder.TextPrinterParser = new TTBPDateTimeFormatterBuilder.TextPrinterParser(MONTH_OF_YEAR, TextStyle.SHORT, TestTextPrinter.PROVIDER)
@@ -98,14 +130,12 @@ object TestTextPrinter {
     assertEquals(buf.toString, "janv.")
   }
 
-  @throws(classOf[Exception])
-  def test_toString1(): Unit = {
+  test("test_toString1") {
     val pp: TTBPDateTimeFormatterBuilder.TextPrinterParser = new TTBPDateTimeFormatterBuilder.TextPrinterParser(MONTH_OF_YEAR, TextStyle.FULL, TestTextPrinter.PROVIDER)
     assertEquals(pp.toString, "Text(MonthOfYear)")
   }
 
-  @throws(classOf[Exception])
-  def test_toString2(): Unit = {
+  test("test_toString2") {
     val pp: TTBPDateTimeFormatterBuilder.TextPrinterParser = new TTBPDateTimeFormatterBuilder.TextPrinterParser(MONTH_OF_YEAR, TextStyle.SHORT, TestTextPrinter.PROVIDER)
     assertEquals(pp.toString, "Text(MonthOfYear,SHORT)")
   }
