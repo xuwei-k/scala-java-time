3498f4a4d1226666de146cb566c980fa8b4f7ffb
Carlos Quiroz <carlos.m.quiroz@gmail.com>
2017-05-07 13:27:05 -0300

    Port TestThaiBuddhistChronology to scalatest

--- shared/src/test/scala/org/threeten/bp/chrono/TestThaiBuddhistChronology.scala
+++ shared/src/test/scala/org/threeten/bp/chrono/TestThaiBuddhistChronology.scala
@@ -31,17 +31,14 @@
  */
 package org.threeten.bp.chrono
 
-import org.testng.Assert.assertEquals
-import org.testng.Assert.assertFalse
-import org.testng.Assert.assertTrue
+import org.scalatest.FunSuite
+
+import org.threeten.bp.AssertionsHelper
 import org.threeten.bp.temporal.ChronoField.DAY_OF_MONTH
 import org.threeten.bp.temporal.ChronoField.DAY_OF_YEAR
 import org.threeten.bp.temporal.ChronoField.MONTH_OF_YEAR
 import org.threeten.bp.temporal.ChronoField.YEAR
 import org.threeten.bp.temporal.ChronoField.YEAR_OF_ERA
-import org.testng.Assert
-import org.testng.annotations.DataProvider
-import org.testng.annotations.Test
 import org.threeten.bp.DateTimeException
 import org.threeten.bp.LocalDate
 import org.threeten.bp.LocalDateTime
@@ -55,110 +52,156 @@ object TestThaiBuddhistChronology {
   private val YDIFF: Int = 543
 }
 
-@Test class TestThaiBuddhistChronology {
-  @Test def test_chrono_byName(): Unit = {
+class TestThaiBuddhistChronology extends FunSuite with AssertionsHelper {
+  test("test_chrono_byName") {
     val c: Chronology = ThaiBuddhistChronology.INSTANCE
     val test: Chronology = Chronology.of("ThaiBuddhist")
-    Assert.assertNotNull(test, "The ThaiBuddhist calendar could not be found byName")
-    Assert.assertEquals(test.getId, "ThaiBuddhist", "ID mismatch")
-    Assert.assertEquals(test.getCalendarType, "buddhist", "Type mismatch")
-    Assert.assertEquals(test, c)
+    assertNotNull(test, "The ThaiBuddhist calendar could not be found byName")
+    assertEquals(test.getId, "ThaiBuddhist", "ID mismatch")
+    assertEquals(test.getCalendarType, "buddhist", "Type mismatch")
+    assertEquals(test, c)
   }
 
-  @DataProvider(name = "samples") private[chrono] def data_samples: Array[Array[AnyRef]] = {
-    Array[Array[AnyRef]](Array(ThaiBuddhistChronology.INSTANCE.date(1 + TestThaiBuddhistChronology.YDIFF, 1, 1), LocalDate.of(1, 1, 1)), Array(ThaiBuddhistChronology.INSTANCE.date(1 + TestThaiBuddhistChronology.YDIFF, 1, 2), LocalDate.of(1, 1, 2)), Array(ThaiBuddhistChronology.INSTANCE.date(1 + TestThaiBuddhistChronology.YDIFF, 1, 3), LocalDate.of(1, 1, 3)), Array(ThaiBuddhistChronology.INSTANCE.date(2 + TestThaiBuddhistChronology.YDIFF, 1, 1), LocalDate.of(2, 1, 1)), Array(ThaiBuddhistChronology.INSTANCE.date(3 + TestThaiBuddhistChronology.YDIFF, 1, 1), LocalDate.of(3, 1, 1)), Array(ThaiBuddhistChronology.INSTANCE.date(3 + TestThaiBuddhistChronology.YDIFF, 12, 6), LocalDate.of(3, 12, 6)), Array(ThaiBuddhistChronology.INSTANCE.date(4 + TestThaiBuddhistChronology.YDIFF, 1, 1), LocalDate.of(4, 1, 1)), Array(ThaiBuddhistChronology.INSTANCE.date(4 + TestThaiBuddhistChronology.YDIFF, 7, 3), LocalDate.of(4, 7, 3)), Array(ThaiBuddhistChronology.INSTANCE.date(4 + TestThaiBuddhistChronology.YDIFF, 7, 4), LocalDate.of(4, 7, 4)), Array(ThaiBuddhistChronology.INSTANCE.date(5 + TestThaiBuddhistChronology.YDIFF, 1, 1), LocalDate.of(5, 1, 1)), Array(ThaiBuddhistChronology.INSTANCE.date(1662 + TestThaiBuddhistChronology.YDIFF, 3, 3), LocalDate.of(1662, 3, 3)), Array(ThaiBuddhistChronology.INSTANCE.date(1728 + TestThaiBuddhistChronology.YDIFF, 10, 28), LocalDate.of(1728, 10, 28)), Array(ThaiBuddhistChronology.INSTANCE.date(1728 + TestThaiBuddhistChronology.YDIFF, 10, 29), LocalDate.of(1728, 10, 29)), Array(ThaiBuddhistChronology.INSTANCE.date(2555, 8, 29), LocalDate.of(2012, 8, 29)))
+  val data_samples: List[(ChronoLocalDate, LocalDate)] = {
+    List(
+      (ThaiBuddhistChronology.INSTANCE.date(1 + TestThaiBuddhistChronology.YDIFF, 1, 1), LocalDate.of(1, 1, 1)),
+      (ThaiBuddhistChronology.INSTANCE.date(1 + TestThaiBuddhistChronology.YDIFF, 1, 2), LocalDate.of(1, 1, 2)),
+      (ThaiBuddhistChronology.INSTANCE.date(1 + TestThaiBuddhistChronology.YDIFF, 1, 3), LocalDate.of(1, 1, 3)),
+      (ThaiBuddhistChronology.INSTANCE.date(2 + TestThaiBuddhistChronology.YDIFF, 1, 1), LocalDate.of(2, 1, 1)),
+      (ThaiBuddhistChronology.INSTANCE.date(3 + TestThaiBuddhistChronology.YDIFF, 1, 1), LocalDate.of(3, 1, 1)),
+      (ThaiBuddhistChronology.INSTANCE.date(3 + TestThaiBuddhistChronology.YDIFF, 12, 6), LocalDate.of(3, 12, 6)),
+      (ThaiBuddhistChronology.INSTANCE.date(4 + TestThaiBuddhistChronology.YDIFF, 1, 1), LocalDate.of(4, 1, 1)),
+      (ThaiBuddhistChronology.INSTANCE.date(4 + TestThaiBuddhistChronology.YDIFF, 7, 3), LocalDate.of(4, 7, 3)),
+      (ThaiBuddhistChronology.INSTANCE.date(4 + TestThaiBuddhistChronology.YDIFF, 7, 4), LocalDate.of(4, 7, 4)),
+      (ThaiBuddhistChronology.INSTANCE.date(5 + TestThaiBuddhistChronology.YDIFF, 1, 1), LocalDate.of(5, 1, 1)),
+      (ThaiBuddhistChronology.INSTANCE.date(1662 + TestThaiBuddhistChronology.YDIFF, 3, 3), LocalDate.of(1662, 3, 3)),
+      (ThaiBuddhistChronology.INSTANCE.date(1728 + TestThaiBuddhistChronology.YDIFF, 10, 28), LocalDate.of(1728, 10, 28)),
+      (ThaiBuddhistChronology.INSTANCE.date(1728 + TestThaiBuddhistChronology.YDIFF, 10, 29), LocalDate.of(1728, 10, 29)),
+      (ThaiBuddhistChronology.INSTANCE.date(2555, 8, 29), LocalDate.of(2012, 8, 29)))
   }
 
-  @Test(dataProvider = "samples") def test_toLocalDate(jdate: ChronoLocalDate, iso: LocalDate): Unit = {
-    assertEquals(LocalDate.from(jdate), iso)
+  test("test_toLocalDate") {
+    data_samples.foreach {
+      case (jdate, iso) =>
+        assertEquals(LocalDate.from(jdate), iso)
+    }
   }
 
-  @Test(dataProvider = "samples") def test_fromCalendrical(jdate: ChronoLocalDate, iso: LocalDate): Unit = {
-    assertEquals(ThaiBuddhistChronology.INSTANCE.date(iso), jdate)
+  test("test_fromCalendrical") {
+    data_samples.foreach {
+      case (jdate, iso) =>
+        assertEquals(ThaiBuddhistChronology.INSTANCE.date(iso), jdate)
+    }
   }
 
-  @DataProvider(name = "badDates") private[chrono] def data_badDates: Array[Array[Any]] = {
-    Array[Array[Any]](Array(1728, 0, 0), Array(1728, -1, 1), Array(1728, 0, 1), Array(1728, 14, 1), Array(1728, 15, 1), Array(1728, 1, -1), Array(1728, 1, 0), Array(1728, 1, 32), Array(1728, 12, -1), Array(1728, 12, 0), Array(1728, 12, 32))
+  val data_badDates: List[(Int, Int, Int)] = {
+    List(
+      (1728, 0, 0),
+      (1728, -1, 1),
+      (1728, 0, 1),
+      (1728, 14, 1),
+      (1728, 15, 1),
+      (1728, 1, -1),
+      (1728, 1, 0),
+      (1728, 1, 32),
+      (1728, 12, -1),
+      (1728, 12, 0),
+      (1728, 12, 32))
   }
 
-  @Test(dataProvider = "badDates", expectedExceptions = Array(classOf[DateTimeException])) def test_badDates(year: Int, month: Int, dom: Int): Unit = {
-    ThaiBuddhistChronology.INSTANCE.date(year, month, dom)
+  test("test_badDates") {
+    data_badDates.foreach {
+      case (year, month, dom) =>
+        assertThrows[DateTimeException] {
+          ThaiBuddhistChronology.INSTANCE.date(year, month, dom)
+        }
+    }
   }
 
-  @Test def test_adjust1(): Unit = {
+  test("test_adjust1") {
     val base: ChronoLocalDate = ThaiBuddhistChronology.INSTANCE.date(1728, 10, 29)
     val test: ChronoLocalDate = base.`with`(TemporalAdjusters.lastDayOfMonth)
     assertEquals(test, ThaiBuddhistChronology.INSTANCE.date(1728, 10, 31))
   }
 
-  @Test def test_adjust2(): Unit = {
+  test("test_adjust2") {
     val base: ChronoLocalDate = ThaiBuddhistChronology.INSTANCE.date(1728, 12, 2)
     val test: ChronoLocalDate = base.`with`(TemporalAdjusters.lastDayOfMonth)
     assertEquals(test, ThaiBuddhistChronology.INSTANCE.date(1728, 12, 31))
   }
 
-  @Test def test_withYear_BE(): Unit = {
+  test("test_withYear_BE") {
     val base: ChronoLocalDate = ThaiBuddhistChronology.INSTANCE.date(2555, 8, 29)
     val test: ChronoLocalDate = base.`with`(YEAR, 2554)
     assertEquals(test, ThaiBuddhistChronology.INSTANCE.date(2554, 8, 29))
   }
 
-  @Test def test_withYear_BBE(): Unit = {
+  test("test_withYear_BBE") {
     val base: ChronoLocalDate = ThaiBuddhistChronology.INSTANCE.date(-2554, 8, 29)
     val test: ChronoLocalDate = base.`with`(YEAR_OF_ERA, 2554)
     assertEquals(test, ThaiBuddhistChronology.INSTANCE.date(-2553, 8, 29))
   }
 
-  @Test def test_withEra_BE(): Unit = {
+  test("test_withEra_BE") {
     val base: ChronoLocalDate = ThaiBuddhistChronology.INSTANCE.date(2555, 8, 29)
     val test: ChronoLocalDate = base.`with`(ChronoField.ERA, ThaiBuddhistEra.BE.getValue)
     assertEquals(test, ThaiBuddhistChronology.INSTANCE.date(2555, 8, 29))
   }
 
-  @Test def test_withEra_BBE(): Unit = {
+  test("test_withEra_BBE") {
     val base: ChronoLocalDate = ThaiBuddhistChronology.INSTANCE.date(-2554, 8, 29)
     val test: ChronoLocalDate = base.`with`(ChronoField.ERA, ThaiBuddhistEra.BEFORE_BE.getValue)
     assertEquals(test, ThaiBuddhistChronology.INSTANCE.date(-2554, 8, 29))
   }
 
-  @Test def test_withEra_swap(): Unit = {
+  test("test_withEra_swap") {
     val base: ChronoLocalDate = ThaiBuddhistChronology.INSTANCE.date(-2554, 8, 29)
     val test: ChronoLocalDate = base.`with`(ChronoField.ERA, ThaiBuddhistEra.BE.getValue)
     assertEquals(test, ThaiBuddhistChronology.INSTANCE.date(2555, 8, 29))
   }
 
-  @Test def test_adjust_toLocalDate(): Unit = {
+  test("test_adjust_toLocalDate") {
     val jdate: ChronoLocalDate = ThaiBuddhistChronology.INSTANCE.date(1726, 1, 4)
     val test: ChronoLocalDate = jdate.`with`(LocalDate.of(2012, 7, 6))
     assertEquals(test, ThaiBuddhistChronology.INSTANCE.date(2555, 7, 6))
   }
 
-  @Test(expectedExceptions = Array(classOf[DateTimeException])) def test_adjust_toMonth(): Unit = {
-    val jdate: ChronoLocalDate = ThaiBuddhistChronology.INSTANCE.date(1726, 1, 4)
-    jdate.`with`(Month.APRIL)
+  test("test_adjust_toMonth") {
+    assertThrows[DateTimeException] {
+      val jdate: ChronoLocalDate = ThaiBuddhistChronology.INSTANCE.date(1726, 1, 4)
+      jdate.`with`(Month.APRIL)
+    }
   }
 
-  @Test def test_LocalDate_adjustToBuddhistDate(): Unit = {
+  test("test_LocalDate_adjustToBuddhistDate") {
     val jdate: ChronoLocalDate = ThaiBuddhistChronology.INSTANCE.date(2555, 10, 29)
     val test: LocalDate = LocalDate.MIN.`with`(jdate)
     assertEquals(test, LocalDate.of(2012, 10, 29))
   }
 
-  @Test def test_LocalDateTime_adjustToBuddhistDate(): Unit = {
+  test("test_LocalDateTime_adjustToBuddhistDate") {
     val jdate: ChronoLocalDate = ThaiBuddhistChronology.INSTANCE.date(2555, 10, 29)
     val test: LocalDateTime = LocalDateTime.MIN.`with`(jdate)
     assertEquals(test, LocalDateTime.of(2012, 10, 29, 0, 0))
   }
 
-  @DataProvider(name = "toString") private[chrono] def data_toString: Array[Array[AnyRef]] = {
-    Array[Array[AnyRef]](Array(ThaiBuddhistChronology.INSTANCE.date(544, 1, 1), "ThaiBuddhist BE 544-01-01"), Array(ThaiBuddhistChronology.INSTANCE.date(2271, 10, 28), "ThaiBuddhist BE 2271-10-28"), Array(ThaiBuddhistChronology.INSTANCE.date(2271, 10, 29), "ThaiBuddhist BE 2271-10-29"), Array(ThaiBuddhistChronology.INSTANCE.date(2270, 12, 5), "ThaiBuddhist BE 2270-12-05"), Array(ThaiBuddhistChronology.INSTANCE.date(2270, 12, 6), "ThaiBuddhist BE 2270-12-06"))
+  val data_toString: List[(ChronoLocalDate, String)] = {
+    List(
+      (ThaiBuddhistChronology.INSTANCE.date(544, 1, 1), "ThaiBuddhist BE 544-01-01"),
+      (ThaiBuddhistChronology.INSTANCE.date(2271, 10, 28), "ThaiBuddhist BE 2271-10-28"),
+      (ThaiBuddhistChronology.INSTANCE.date(2271, 10, 29), "ThaiBuddhist BE 2271-10-29"),
+      (ThaiBuddhistChronology.INSTANCE.date(2270, 12, 5), "ThaiBuddhist BE 2270-12-05"),
+      (ThaiBuddhistChronology.INSTANCE.date(2270, 12, 6), "ThaiBuddhist BE 2270-12-06"))
   }
 
-  @Test(dataProvider = "toString") def test_toString(jdate: ChronoLocalDate, expected: String): Unit = {
-    assertEquals(jdate.toString, expected)
+  test("test_toString") {
+    data_toString.foreach {
+      case (jdate, expected) =>
+        assertEquals(jdate.toString, expected)
+    }
   }
 
-  @Test def test_Chrono_range(): Unit = {
+  test("test_Chrono_range") {
     val minYear: Long = LocalDate.MIN.getYear + TestThaiBuddhistChronology.YDIFF
     val maxYear: Long = LocalDate.MAX.getYear + TestThaiBuddhistChronology.YDIFF
     assertEquals(ThaiBuddhistChronology.INSTANCE.range(YEAR), ValueRange.of(minYear, maxYear))
@@ -168,11 +211,11 @@ object TestThaiBuddhistChronology {
     assertEquals(ThaiBuddhistChronology.INSTANCE.range(MONTH_OF_YEAR), MONTH_OF_YEAR.range)
   }
 
-  @Test def test_equals_true(): Unit = {
+  test("test_equals_true") {
     assertTrue(ThaiBuddhistChronology.INSTANCE == ThaiBuddhistChronology.INSTANCE)
   }
 
-  @Test def test_equals_false(): Unit = {
+  test("test_equals_false") {
     assertFalse(ThaiBuddhistChronology.INSTANCE == IsoChronology.INSTANCE)
   }
 }
